c44bf8d040e4a1f3dfcafdd4f2dd6a8c08c51078
Tinashe Nyakutetepa <tinashenyakutetepa@outlook.com>
2021-10-28 10:51:30 +0400

    apis

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.stockList_api, name="stocklist-api"),    
+    path('',views.stockList_api, name="stocklist-api"),  
+    path('stock-detail/<int:pk>/', views.stockDetail, name="stockDetail-api"), 
+    path('delete-stock/<int:pk>/', views.deleteStock, name="deleteStock-api"),  
+    path('create-stock/', views.createStock, name="createStock-api"),
+    path('update-stock/<int:pk>/', views.updateStock, name="updateStock-api"),
 ]
--- api/views.py
+++ api/views.py
@@ -10,3 +10,37 @@ def stockList_api(request):
 	stockList = Stock.objects.all().order_by('-id')
 	serializer = StockSerializer(stockList, many=True)
 	return Response(serializer.data)
+
+@api_view(['GET'])
+def stockDetail(request, pk):
+	stock = Stock.objects.get(id=pk)
+	serializer = StockSerializer(stock, many=False)
+	return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteStock(request, pk):
+	stock = Stock.objects.get(id=pk)
+	stock.delete()
+
+	return Response('Item succsesfully delete!')
+
+
+@api_view(['POST'])
+def createStock(request):
+	serializer = StockSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+	
+
+@api_view(['POST'])
+def updateStock(request, pk):
+	stock = Stock.objects.get(id=pk)
+	serializer = StockSerializer(instance=stock, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
